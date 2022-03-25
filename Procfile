830cc31d15348be61185752a4b53f8fad3bb595a
deviljin1009 <jiajinpang@gmail.com>
2022-03-25 19:05:39 +0800

    Change html to php

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./vendor/bin/heroku-php-apache2 fyp webpage/
+web: vendor/bin/heroku-php-apache2 
--- fyp webpage/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('include/session.php');
-    unset($_SESSION['id']);
-    unset($_SESSION['name']);
-    
-    header("Location:login.php");
-?>
