43b08d2fd287d3efbc88f845f96b0ad023684184
Pang Jia Jin <86699859+deviljin1009@users.noreply.github.com>
2022-03-26 16:08:24 +0800

    Update navigate.php

--- navigate.php
+++ navigate.php
@@ -3,12 +3,6 @@
 include_once("include/config.php");
 include_once("include/session.php");
 
-
-$blink = $_GET['blink'];
-$method = $_GET['method'];
-$_SESSION['blinks']=$_GET['blink'];
-$_SESSION['method']=$_GET['method'];
-
 header("Location:questionnaire.php");
   
 ?>
