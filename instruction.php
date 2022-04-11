4b22ee8633ebd5c7806aa7bfa13c1a5c9acae5e7
Pang Jia Jin <86699859+deviljin1009@users.noreply.github.com>
2022-04-11 20:01:40 +0800

    Update instruction.php

--- instruction.php
+++ instruction.php
@@ -21,7 +21,10 @@
 echo "<div class=\"box\">";
 
 echo "<div class=\"header\">Please Follow the Instruction Below</div>";
-echo "<div class=\"instruction\">You will be given 2 minute to read through an article. Your stress level will be determined by analyzing your eye blinking rate throughout the event. </div>";
+echo "<div class=\"instruction\">You will be given 2 minute to read through an article. Your stress level will be determined by analyzing your eye blinking rate throughout the event. 
+        <br>
+        Note: Please avoid wearing glasses and keep you head stay still for more accurate detection. Due to the delayness of server, please wait for 10-20 seconds for the webcam frame to show up and refresh the page.
+        </div>";
 
 echo "<div class=\"video\">";
     echo "<video width=\"750px\" height=\"600px\" autoplay muted loop>
